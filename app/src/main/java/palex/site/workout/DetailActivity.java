d170e9c27562354416a263a13764810bbf622ef3
Home <dumadumae@mail.ru>
2019-02-21 22:20:59 +0300

    Добавил вызов и контент в фрагмент

--- app/src/main/java/palex/site/workout/DetailActivity.java
+++ app/src/main/java/palex/site/workout/DetailActivity.java
@@ -9,5 +9,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);    //получаем ссылку на фрагмент через вызов метода findFragmentById() у диспетчера фрагментов
+        frag.setWorkout(1);
     }
 }
--- app/src/main/java/palex/site/workout/WorkoutDetailFragment.java
+++ app/src/main/java/palex/site/workout/WorkoutDetailFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
 
 
     public WorkoutDetailFragment() {
@@ -24,6 +27,25 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();    //Версию суперкласса нужно вызывать всегда при реализации любых методов жизненного цикла фрагмента.
+        View view = getView();  //получаем корневой объект View фрагмента.
+        if (view != null) { // далее view используем для получения ссылок на надписи.
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+
+        }
+    }
+
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
     }
 
 }
